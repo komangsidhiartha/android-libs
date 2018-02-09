93f821f1ffa093c7ebe8815d13ba15bc870b9577
Sidhi Artha <sidhi_artha@mail.ugm.ac.id>
2018-02-09 18:05:03 +0700

    tambah base support fragment

---
+++ src/main/java/com/sidhiartha/libs/fragments/BaseSupportFragment.kt
@@ -0,0 +1,27 @@
+package com.sidhiartha.libs.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by sidhiartha on 09/02/18.
+ */
+abstract class BaseSupportFragment : Fragment() {
+
+    protected abstract fun layoutResource(): Int
+
+    protected abstract fun viewDidLoad()
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(layoutResource(), null)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewDidLoad()
+    }
+}
