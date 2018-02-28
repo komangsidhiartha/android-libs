4e32473e63fc6cf99807f11f70ce427b3f94b15a
Sidhi Artha <sidhi_artha@mail.ugm.ac.id>
2018-02-28 16:46:13 +0700

    perbaiki dikit

--- src/main/java/com/sidhiartha/libs/adapters/RecyclerAdapter.kt
+++ src/main/java/com/sidhiartha/libs/adapters/RecyclerAdapter.kt
@@ -2,27 +2,39 @@ package com.sidhiartha.libs.adapters
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by sidhiartha on 28/02/18.
  */
-abstract class RecyclerAdapter<E, V: ViewHolder<E>>(private val context: Context, protected var items: ArrayList<E>): RecyclerView.Adapter<V>()
+abstract class RecyclerAdapter<E, out V: ItemView<E>>(protected val context: Context, protected var items: ArrayList<E>): RecyclerView.Adapter<RecyclerAdapter<E,V>.BaseViewHolder>()
 {
-    protected abstract fun layoutResource(): Int
+    protected abstract fun inflateItemView(parent: ViewGroup?): V
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder {
+        return BaseViewHolder(inflateItemView(parent))
+    }
 
     override fun getItemCount(): Int
     {
         return items.size
     }
 
-    override fun onBindViewHolder(holder: V, position: Int)
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int)
     {
         holder.bind(items[position])
     }
+
+    inner class BaseViewHolder(private val view: V): RecyclerView.ViewHolder(view)
+    {
+        fun bind(item: E)
+        {
+            view.bind(item)
+        }
+    }
 }
 
-abstract class ViewHolder<in E>(itemView: View) : RecyclerView.ViewHolder(itemView)
+abstract class ItemView<in E>(context: Context) : ViewGroup(context)
 {
     abstract fun bind(item: E)
 }
