9cf2ce534f181cce46bfea09da124d5d9ca441f8
Sidhi Artha <sidhi_artha@mail.ugm.ac.id>
2018-07-07 22:09:11 +0700

    change fun to val

--- src/main/java/com/sidhiartha/libs/dialogs/BaseDialog.kt
+++ src/main/java/com/sidhiartha/libs/dialogs/BaseDialog.kt
@@ -10,12 +10,12 @@ import android.view.ViewGroup
  * Created by sidhiartha on 12/02/18.
  */
 abstract class BaseDialog : DialogFragment() {
-    protected abstract fun layoutResource(): Int
+    protected abstract val layoutResource: Int
 
     protected abstract fun viewDidLoad()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater!!.inflate(layoutResource(), container)
+        return inflater!!.inflate(layoutResource, container)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
