9a1af270e554fa3451ccba92c666b0c4a433f19e
Sidhi Artha <sidhi_artha@mail.ugm.ac.id>
2018-07-11 00:31:38 +0700

    button click track

---
+++ src/main/java/com/sidhiartha/libs/views/ButtonExtension.kt
@@ -0,0 +1,15 @@
+package com.sidhiartha.libs.views
+
+import android.widget.Button
+import com.sidhiartha.libs.BaseApplication
+import org.jetbrains.anko.onClick
+
+fun Button.onTrackedClick(callback: () -> Unit)
+{
+
+    this.onClick {
+        it as Button
+        BaseApplication.instance?.sendEvent("${it.id}", "${it.text}")
+        callback()
+    }
+}
