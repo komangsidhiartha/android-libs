88bb3a2a6309d2afe9785cd7c17e3871cff57a76
Sidhi Artha <sidhi_artha@mail.ugm.ac.id>
2018-09-09 16:59:45 +0700

    call api with JSON body params

--- src/main/java/com/sidhiartha/libs/networks/BaseAPI.kt
+++ src/main/java/com/sidhiartha/libs/networks/BaseAPI.kt
@@ -32,13 +32,9 @@ abstract class BaseAPI
 
     abstract val method: APIMethod
 
-    abstract val params: List<Pair<String, String>>
-
     abstract val path: String
 
-//    protected open fun jsonParams() : String {
-//        return ""
-//    }
+    abstract val params : String
 
     protected fun extract(queryParams: List<Pair<String, String>>): String
     {
@@ -79,51 +75,40 @@ abstract class BaseAPI
         }
     }
 
-//    fun <T> executeJson(kelas: Class<T>, handler: (response: T?, errorMessage: String?) -> Unit) {
-//        val localHandler = { request: Request, response: Response, result: Result<Json, FuelError> ->
-//            val (json, error) = result
-//
-//            Log.w("Network Manager", "request $request")
-//            Log.w("Network Manager", "response $response")
-//            Log.w("Network Manager", "json $json")
-//            Log.w("Network Manager", "error $error")
-//            if (error != null) {
-//                handler(null, error.localizedMessage)
-//            } else {
-//                handler(GSONManager.fromJson(json!!.obj(), kelas), null)
-//            }
-//        }
-//
-//        when (method) {
-//            APIMethod.GET -> get(localHandler)
-//            APIMethod.DELETE -> delete(localHandler)
-//            APIMethod.POST -> postJson(localHandler)
-//            APIMethod.PUT -> put(localHandler)
-//            else -> Log.i("TAG", "unsupported $method method")
-//        }
-//    }
-
     private fun get(handler: (request: Request, response: Response, result: Result<Json, FuelError>) -> Unit)
     {
+        appendHeaderWithAcceptJson()
         "$basePath$path".httpGet().header(headers).responseJson(handler)
     }
 
     private fun delete(handler: (request: Request, response: Response, result: Result<Json, FuelError>) -> Unit)
     {
-        "$basePath$path".httpDelete(params).header(headers).responseJson(handler)
+        appendHeaderWithJsonSpecific()
+        "$basePath$path".httpDelete().body(params).header(headers).responseJson(handler)
     }
-
-//    private fun postJson(handler: (request: Request, response: Response, result: Result<Json, FuelError>) -> Unit) {
-//        "$basePath$path".httpPost().body(jsonParams()).header(headers?.plus(mapOf("Content-Type" to "application/json"))).responseJson(handler)
-//    }
 
     private fun post(handler: (request: Request, response: Response, result: Result<Json, FuelError>) -> Unit)
     {
-        "$basePath$path".httpPost(params).header(headers).responseJson(handler)
+        appendHeaderWithJsonSpecific()
+        "$basePath$path".httpPost().body(params).header(headers).responseJson(handler)
     }
 
     private fun put(handler: (request: Request, response: Response, result: Result<Json, FuelError>) -> Unit)
     {
-        "$basePath$path".httpPut(params).header(headers).responseJson(handler)
+        appendHeaderWithJsonSpecific()
+        "$basePath$path".httpPut().body(params).header(headers).responseJson(handler)
+    }
+
+    private fun appendHeaderWithJsonSpecific() {
+        appendHeaderWithJsonContentType()
+        appendHeaderWithAcceptJson()
+    }
+
+    private fun appendHeaderWithJsonContentType() {
+        headers?.plus(mapOf("Content-Type" to "application/json"))
+    }
+
+    private fun appendHeaderWithAcceptJson() {
+        headers?.plus(mapOf("Accept" to "application/json"))
     }
 }
