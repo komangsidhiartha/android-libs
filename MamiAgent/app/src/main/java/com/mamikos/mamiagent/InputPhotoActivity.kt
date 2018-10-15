741154c3ef364e063c5d809846357158737e8394
Dedi Doot <dedidoot@gmail.com>
2018-10-15 16:04:45 +0700

    up

--- MamiAgent/app/src/main/java/com/mamikos/mamiagent/InputPhotoActivity.kt
+++ MamiAgent/app/src/main/java/com/mamikos/mamiagent/InputPhotoActivity.kt
@@ -12,10 +12,6 @@ import android.support.v4.content.ContextCompat
 import android.support.v4.content.FileProvider
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
 import com.mamikos.mamiagent.adapters.AddPhotoAdapter
 import com.mamikos.mamiagent.entities.MediaEntity
 import com.mamikos.mamiagent.entities.RoomEntity
